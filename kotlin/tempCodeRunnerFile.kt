f57f863395c32830897630f6d3f8a98d58cbd49e
kangwlgns <71175483+kangwlgns@users.noreply.github.com>
2023-03-06 18:34:04 +0900

    Delete kotlin directory

--- kotlin/tempCodeRunnerFile.kt
+++
@@ -1,7 +0,0 @@
-
-
-fun main() = System.`in`.bufferedReader().run {
-    val sum = {a: Int, b: Boolean -> (if (b) 1 else 2) + a}
-    
-    print(sum(2, true))
-}
--- kotlin/test.kt
+++
@@ -1,8 +0,0 @@
-fun main() = System.`in`.bufferedReader().run {
-    println("         ,r'\"7")
-    println("r`-_   ,'  ,/")
-    println(" \\. \". L_r'")
-    println("   `~\\/")
-    println("      |")
-    println("      |")
-}
